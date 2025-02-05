46f6e0983d18910116485b660f208e484e36e659
Jonna Wiro <jonna.wiro@student.careeria.fi>
2025-02-05 18:26:02 +0200

    Lisäystoiminto

--- NorthwindRestApi/Controllers/CustomersController.cs
+++ NorthwindRestApi/Controllers/CustomersController.cs
@@ -15,8 +15,55 @@ namespace NorthwindRestApi.Controllers
         [HttpGet]
         public ActionResult GetAllCustomers()
         {
-            var asiakkaat = db.Customers.ToList();
-            return Ok(asiakkaat);
+            try
+            {
+                var asiakkaat = db.Customers.ToList();
+                return Ok(asiakkaat);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + ex.InnerException); //InnerException on tarkempi kuin esim. Message
+            }
         }
+
+
+        // Hakee yhden asiakkaan pääavaimella
+        [HttpGet("{id}")]
+        public ActionResult GetOneCustomerById(string id)
+        {
+            try
+            {
+                var asiakas = db.Customers.Find(id);
+                if (asiakas != null)
+                {
+                    return Ok(asiakas);
+                }
+                else
+                {
+                    return NotFound($"Asiakasta id:llä {id} ei löydy."); // string interpolation
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e);
+            }
+        }
+
+        // Uuden lisääminen
+        [HttpPost]
+        public ActionResult AddNew([FromBody] Customer cust) //Ottaa vastaan Http -body -osiosta Customer (vahva tyyppi) -luokkaa vastaavan olion
+        {
+            try
+            {
+                db.Customers.Add(cust);
+                db.SaveChanges();
+                return Ok("Lisättiin uusi asiakas " + cust.CompanyName);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+            }
+        }
+
     }
 }
